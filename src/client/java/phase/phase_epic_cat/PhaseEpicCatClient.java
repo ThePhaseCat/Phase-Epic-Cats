8db34d7f5b101fdd879e810a50313bc0df05bc07
ThePhaseCat <alexmgamer10@gmail.com>
2024-04-06 16:25:33 -0600

    first push?

---
+++ src/client/java/phase/phase_epic_cat/PhaseEpicCatClient.java
@@ -0,0 +1,10 @@
+package phase.phase_epic_cat;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PhaseEpicCatClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/phase/phase_epic_cat/PhaseEpicCatDataGenerator.java
@@ -0,0 +1,11 @@
+package phase.phase_epic_cat;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PhaseEpicCatDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
