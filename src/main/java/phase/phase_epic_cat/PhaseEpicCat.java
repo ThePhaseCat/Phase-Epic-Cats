7dee0109ff21dda74a49da6903db6b8a2934ec0e
ThePhaseCat <alexmgamer10@gmail.com>
2024-04-11 20:25:22 -0600

    redid everything (fun)
    -redid everything block and item related into the fabric system
    -started working on datagen setup

--- src/main/java/phase/phase_epic_cat/PhaseEpicCat.java
+++ src/main/java/phase/phase_epic_cat/PhaseEpicCat.java
@@ -3,7 +3,12 @@ package phase.phase_epic_cat;
 import io.wispforest.owo.registration.reflect.FieldRegistrationHandler;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
@@ -12,7 +17,6 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import phase.phase_epic_cat.items.ItemInit;
 
 public class PhaseEpicCat implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
@@ -21,20 +25,31 @@ public class PhaseEpicCat implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("phaseepiccat");
 	public static final String MOD_ID = "phase_epic_cats";
 
+
+	//blocks and items
+	public static final Item TEST_ITEM = new Item(new FabricItemSettings());
+	public static final Block TEST_BLOCK  = new Block(FabricBlockSettings.create().strength(1.0f));
+
+
 	private static final ItemGroup EpicCatsItemGroup = FabricItemGroup.builder()
-			.icon(() -> new ItemStack(ItemInit.TEST))
-			.displayName(Text.translatable("Phase's Epic Cats"))
-			.entries((context, entries) ->{
-				entries.add(new ItemStack(ItemInit.TEST));
-			})
-			.build();
+			.icon(() -> new ItemStack(TEST_ITEM))
+					.displayName(Text.translatable("Phase's Epic Cats"))
+					.entries((context, entries) ->{
+						entries.add(new ItemStack(TEST_ITEM));
+						entries.add(new ItemStack(TEST_BLOCK));
+					})
+					.build();
 
 	@Override
 	public void onInitialize()
 	{
 		LOGGER.info("Phase's Epic Cat Mod Main initializing!");
-		FieldRegistrationHandler.register(ItemInit.class, MOD_ID, false);
+
+		Registry.register(Registries.ITEM, new Identifier(MOD_ID, "test_item"), TEST_ITEM);
+		Registry.register(Registries.BLOCK, new Identifier(MOD_ID, "test_block"), TEST_BLOCK);
+		Registry.register(Registries.ITEM, new Identifier(MOD_ID, "test_block"), new BlockItem(TEST_BLOCK, new FabricItemSettings()));
 		Registry.register(Registries.ITEM_GROUP, new Identifier(MOD_ID, "phase_epic_cats"), EpicCatsItemGroup);
+
 		LOGGER.info("Phase's Epic Cat Mod Main initialized!");
 	}
 }
