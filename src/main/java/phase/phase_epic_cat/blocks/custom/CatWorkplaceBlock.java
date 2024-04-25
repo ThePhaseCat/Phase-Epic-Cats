588ec9f3904507292cbc96b8404af96a2ca091d3
ThePhaseCat <alexmgamer10@gmail.com>
2024-04-25 15:43:04 -0600

    added a variable to CatWorkplaceBlock
    -IS_CAT_PRESENT is used to check if a cat is present with this cat workplace block
    -it does nothing

--- src/main/java/phase/phase_epic_cat/blocks/custom/CatWorkplaceBlock.java
+++ src/main/java/phase/phase_epic_cat/blocks/custom/CatWorkplaceBlock.java
@@ -2,7 +2,10 @@ package phase.phase_epic_cat.blocks.custom;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
@@ -12,16 +15,35 @@ import net.minecraft.world.World;
 public class CatWorkplaceBlock extends Block
 {
 
+    public static final BooleanProperty IS_CAT_PRESENT = BooleanProperty.of("is_cat_present");
+
     public CatWorkplaceBlock(Settings settings) {
         super(settings);
+        setDefaultState(getDefaultState().with(IS_CAT_PRESENT, false));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(IS_CAT_PRESENT);
     }
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
         if (!world.isClient) {
             player.sendMessage(Text.literal("Hello, world!"), false);
+            world.setBlockState(pos, state.with(IS_CAT_PRESENT, true));
         }
 
         return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
+        if (world.getBlockState(pos).get(IS_CAT_PRESENT)){
+            System.out.println("CAT IS PRESENT!!!");
+        }
+
+        world.setBlockState(pos, state.with(IS_CAT_PRESENT, false));
+        super.onSteppedOn(world, pos, state, entity);
     }
 }
